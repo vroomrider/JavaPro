abb70563009a2297d3949ae411b5c1e9a6de61bd
PradeepNaikR <pradeep.naik@riflexions.com>
2017-12-14 13:51:29 +0530

    Initial Commit!

---
+++ src/main/java/com/javapro/App.java
@@ -0,0 +1,27 @@
+package com.javapro;
+
+/**
+ * @AccessSpecifiers can be only
+ * 
+ *                   public abstract
+ * 
+ * @IndustryStandard naming convention App
+ * 
+ *                   we can have @CamelCaseLetters also
+ */
+public class App {
+	/**
+	 * 
+	 * if @Main method does not exist it will not execute but will get compiled!
+	 * we can @OverLoad the main method
+	 * 
+	 * @public & @static because O.S
+	 */
+	public static void main(String[] args) {
+		
+		/**
+		 * @System
+		 */
+		System.out.println("Hello World!");
+	}
+}
